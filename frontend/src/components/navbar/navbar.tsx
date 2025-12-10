fc7887ba16f91278aee018a0637d4f29fa064de2
Ahmed Klai <ahmedklai.connect@gmail.com>
2025-12-10 20:44:51 +0100

    (feat):Basic nabar naviguation

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,11 +1,29 @@
 import style from './navbar.module.css';
+import Logo from '../../assets/logo.png';
 
 // Navbar component
 const Navbar = () => {
   return (
     <>
         <nav className={style.navContainer}>
-            
+          <div className={style.navbarContent}>
+            <a>
+              <img src={Logo} alt="App Logo that redirects href home on click" className={style.logo} />
+            </a>
+
+            <div className={style.navas}>
+              <button>Search</button>
+              <button>Map</button>
+              <a href="/PostFoundItem">Post Found Item</a>
+              <a href="/ReportLostItem">Report Lost Item</a>
+              <a href="/Help">Help</a>
+            </div>
+
+            <div className={style.navActions}>
+              <button className={style.themeToggle}>Theme Toggle</button>
+              <button className={style.profile}>Profile</button>
+            </div>
+          </div>
         </nav>
     </>
   );
